e2d3b1cd68175c485461cb16d2fc6c0fe76d7aa3
Bimendra Bandara <bimendra.bandara@velaris.io>
2023-05-01 12:22:10 +0800

    Hard code Rapid API key

--- api/src/exercises/exercises.module.ts
+++ api/src/exercises/exercises.module.ts
@@ -9,7 +9,7 @@ import { HttpModule } from '@nestjs/axios';
       baseURL: 'https://exercisedb.p.rapidapi.com/exercises',
       headers: {
         'content-type': 'application/octet-stream',
-        'X-RapidAPI-Key': process.env.EXERCISE_DB_API_KEY,
+        'X-RapidAPI-Key': '64540054d3mshd0d575529a3ee04p1dcd48jsn4a88be98b18e',
         'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
       },
     }),
