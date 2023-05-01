afa6e8c908d9aede543ebc33f967421ace5ff844
Bimendra Bandara <bimendra.bandara@velaris.io>
2023-05-01 12:16:41 +0800

    Import exercise module into the app module

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,8 +3,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
+import { ExercisesModule } from './exercises/exercises.module';
 @Module({
-  imports: [PrismaModule, AuthModule],
+  imports: [PrismaModule, AuthModule, ExercisesModule],
   controllers: [AppController],
   providers: [AppService],
 })
